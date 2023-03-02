4d2481f5665ec70dfd8e344c13190d825957c229
Arun Arram <arun.arram@gmail.com>
2023-03-02 09:43:02 +0530

    Added Exclaimation mark in Greeting.java

--- Greeting.java
+++ Greeting.java
@@ -3,6 +3,6 @@ public class Greeting{
 	public static void main(String args[]){
 		Scanner sc= new Scanner(System.in);
 		String name= sc.nextLine();
-		System.out.println("Hi "+name+"!!");
+		System.out.println("Hi "+name+"!!!");
 	}
 }
